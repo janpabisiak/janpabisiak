3b5ede33fac23a2c320dcc3ccfa449b0a5de71f1
Jan P. Pabisiak <contact@janpabisiak.com>
2024-09-14 01:41:00 +0200

    fixes and optimization

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,26 @@
+'use strict';
+
 const toggleButton = document.querySelector('.toggle-button');
-const navbarLinks = document.querySelector('.navbar-links');
+const navbar = document.querySelector('.navbar-links');
+const navbarLinks = document.querySelectorAll('.navbar-links ul li a');
+const percOfScrolledElm = document.querySelector('.percOfScrolled');
+const sectionPositions = [-999];
 let headerHeight;
-const sectionsOffsetTop = [];
 let fullScroll;
 let currentScroll;
-let percOfScrolledContent;
-let previousClickedNavLink;
+let percOfScrolled;
+
+const setActiveNavBarLink = () => {
+	let currentSection;
+	for (let i = 1; i < sectionPositions.length; i++) {
+		if (sectionPositions[i - 1] < currentScroll && currentScroll < sectionPositions[i]) currentSection = i - 1;
+		else if (currentScroll > sectionPositions[i]) currentSection = i;
+	}
+	navbarLinks.forEach((link, i) => {
+		if (currentSection === i) link.classList.add('active');
+		else link.classList.remove('active');
+	});
+};
 
 window.onload = () => {
 	document.querySelector('.loading-container').classList.add('hidden');
@@ -19,56 +34,25 @@ window.onload = () => {
 	fullScroll = Math.round(document.documentElement.scrollHeight - document.documentElement.clientHeight);
 	headerHeight = document.querySelector('header').offsetHeight;
 	document.querySelectorAll('.section-title').forEach((section) => {
-		sectionsOffsetTop.push({
-			section: section.textContent.toLowerCase().replaceAll(' ', '-'),
-			position: section.offsetTop - headerHeight,
-		});
+		sectionPositions.push(section.offsetTop - headerHeight);
 	});
+	sectionPositions[4] = sectionPositions[4] - window.innerHeight;
 };
 
 document.addEventListener('scroll', () => {
 	currentScroll = Math.round(window.scrollY);
-	if (currentScroll > headerHeight && currentScroll < sectionsOffsetTop[0].position) {
-		navbarLinks.childNodes[1].childNodes[1].firstChild.classList.add('active');
-	}
-	if (currentScroll > sectionsOffsetTop[0].position && currentScroll < sectionsOffsetTop[1].position) {
-		navbarLinks.childNodes[1].childNodes[1].firstChild.classList.remove('active');
-		navbarLinks.childNodes[1].childNodes[3].firstChild.classList.add('active');
-	}
-	if (currentScroll > sectionsOffsetTop[1].position && currentScroll < sectionsOffsetTop[2].position) {
-		navbarLinks.childNodes[1].childNodes[3].firstChild.classList.remove('active');
-		navbarLinks.childNodes[1].childNodes[5].firstChild.classList.add('active');
-	}
-	if (currentScroll > sectionsOffsetTop[2].position && currentScroll < sectionsOffsetTop[3].position) {
-		navbarLinks.childNodes[1].childNodes[5].firstChild.classList.remove('active');
-		navbarLinks.childNodes[1].childNodes[7].firstChild.classList.add('active');
-	}
+	setActiveNavBarLink();
 
-	percOfScrolledContent = (currentScroll / fullScroll) * 100;
-	document.querySelector('.percOfScrolledContent').style.width = `${percOfScrolledContent}%`;
+	percOfScrolled = (currentScroll / fullScroll) * 100;
+	percOfScrolledElm.style.width = `${percOfScrolled}%`;
 });
 
 toggleButton.addEventListener('click', (e) => {
 	e.preventDefault();
-	navbarLinks.classList.toggle('active');
+	navbar.classList.toggle('active');
 });
 
-// navbarLinks.addEventListener('click', (e) => {
-// 	if (e.target.nodeName != 'A') return;
-// 	e.target.classList.add('active');
-// 	if (previousClickedNavLink !== undefined) previousClickedNavLink.classList.remove('active');
-// 	previousClickedNavLink = e.target;
-// });
-
-window.innerWidth <= 768 ? navbarLinks.childNodes[1].childNodes[11].firstChild.classList.remove('btn') : '';
-
-setTimeout(() => {
-	const emaiLEl = document.querySelector('#email');
-	const address = 'contact@janpabisiak.com';
-	emaiLEl.href = `mailto:${address}`;
-	let r = document.createTextNode(address);
-	emaiLEl.appendChild(r);
-}, 1000);
+window.innerWidth <= 768 ? navbarLinks[navbarLinks.length - 1].classList.remove('btn') : '';
 
 const year = new Date().getFullYear();
 document.querySelector('.age').textContent = year - 2004;
