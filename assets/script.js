753eb51244c8f8152b0173071de8c05c906591bb
Jan P. Pabisiak <contact@janpabisiak.com>
2024-09-14 00:38:34 +0200

    small changes, images compressed

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
+let headerHeight;
+const sectionsOffsetTop = [];
 let fullScroll;
 let currentScroll;
 let percOfScrolledContent;
@@ -15,10 +17,33 @@ window.onload = () => {
 	document.querySelector('#contact').classList.remove('hidden');
 	document.querySelector('footer').classList.remove('hidden');
 	fullScroll = Math.round(document.documentElement.scrollHeight - document.documentElement.clientHeight);
+	headerHeight = document.querySelector('header').offsetHeight;
+	document.querySelectorAll('.section-title').forEach((section) => {
+		sectionsOffsetTop.push({
+			section: section.textContent.toLowerCase().replaceAll(' ', '-'),
+			position: section.offsetTop - headerHeight,
+		});
+	});
 };
 
 document.addEventListener('scroll', () => {
 	currentScroll = Math.round(window.scrollY);
+	if (currentScroll > headerHeight && currentScroll < sectionsOffsetTop[0].position) {
+		navbarLinks.childNodes[1].childNodes[1].firstChild.classList.add('active');
+	}
+	if (currentScroll > sectionsOffsetTop[0].position && currentScroll < sectionsOffsetTop[1].position) {
+		navbarLinks.childNodes[1].childNodes[1].firstChild.classList.remove('active');
+		navbarLinks.childNodes[1].childNodes[3].firstChild.classList.add('active');
+	}
+	if (currentScroll > sectionsOffsetTop[1].position && currentScroll < sectionsOffsetTop[2].position) {
+		navbarLinks.childNodes[1].childNodes[3].firstChild.classList.remove('active');
+		navbarLinks.childNodes[1].childNodes[5].firstChild.classList.add('active');
+	}
+	if (currentScroll > sectionsOffsetTop[2].position && currentScroll < sectionsOffsetTop[3].position) {
+		navbarLinks.childNodes[1].childNodes[5].firstChild.classList.remove('active');
+		navbarLinks.childNodes[1].childNodes[7].firstChild.classList.add('active');
+	}
+
 	percOfScrolledContent = (currentScroll / fullScroll) * 100;
 	document.querySelector('.percOfScrolledContent').style.width = `${percOfScrolledContent}%`;
 });
@@ -27,14 +52,15 @@ toggleButton.addEventListener('click', (e) => {
 	e.preventDefault();
 	navbarLinks.classList.toggle('active');
 });
-window.innerWidth < 768 && document.querySelector('.contact-link').classList.remove('btn');
 
-navbarLinks.addEventListener('click', (e) => {
-	if (e.target.nodeName != 'A') return;
-	e.target.classList.add('active');
-	if (previousClickedNavLink !== undefined) previousClickedNavLink.classList.remove('active');
-	previousClickedNavLink = e.target;
-});
+// navbarLinks.addEventListener('click', (e) => {
+// 	if (e.target.nodeName != 'A') return;
+// 	e.target.classList.add('active');
+// 	if (previousClickedNavLink !== undefined) previousClickedNavLink.classList.remove('active');
+// 	previousClickedNavLink = e.target;
+// });
+
+window.innerWidth <= 768 ? navbarLinks.childNodes[1].childNodes[11].firstChild.classList.remove('btn') : '';
 
 setTimeout(() => {
 	const emaiLEl = document.querySelector('#email');
