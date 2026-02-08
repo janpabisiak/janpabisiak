e626e97eb1999160ed7e228e44f8314f5144b7bd
Jan Pabisiak <contact@janpabisiak.com>
2026-02-08 20:38:24 +0100

    redesign project section

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,8 @@
 const loadingEl = document.querySelector('.loading');
 const hamburgerEl = document.querySelector('.hamburger');
 const navbarEl = document.querySelector('.navbar__links');
-const navbarLinksEls = document.querySelectorAll('.navbar__links ul li a');
-const heroSectionBackgroundEl = getComputedStyle(document.querySelector('#hero'), ':before').getPropertyValue('content');
+const navbarLinksEls = document.querySelectorAll('.navbar__links li a');
+const skillSectionEl = document.querySelector('#skills');
 const skillItemsEls = document.querySelectorAll('.skills__list__item');
 
 const year = new Date().getFullYear();
@@ -13,8 +13,6 @@ let headerHeight;
 let fullScroll;
 let currentScroll;
 let percentageScrolled;
-
-window.innerWidth <= 870 ? navbarLinksEls[navbarLinksEls.length - 1].classList.remove('btn') : '';
 
 const displayPage = () => {
 	const sectionIds = ['header', 'hero', 'about', 'skills', 'projects', 'contact', 'footer'];
@@ -49,6 +47,27 @@ const setActiveNavBarLink = () => {
 	});
 };
 
+const scrollImage = (imgEl, scrollUp, animationDuration, altImgPrefix) => {
+	if (window.innerWidth <= 1300) return;
+
+	const { height } = imgEl.getBoundingClientRect();
+
+	if (scrollUp) {
+		imgEl.style.height = `${height}px`;
+		imgEl.style.objectFit = 'cover';
+		imgEl.src = `./assets/images/projects/${altImgPrefix}-full.webp`;
+		imgEl.style.objectPosition = 'center top';
+		imgEl.offsetHeight;
+		imgEl.style.transition = `object-position ${animationDuration}s linear`;
+		imgEl.style.objectPosition = 'center bottom';
+	} else {
+		imgEl.style.objectPosition = 'center bottom';
+		imgEl.offsetHeight;
+		imgEl.style.transition = `object-position ${animationDuration}s linear`;
+		imgEl.style.objectPosition = 'center top';
+	}
+};
+
 const skillItemsObserver = new IntersectionObserver((entries) => {
 	entries.forEach((entry, i) => {
 		if (entry.isIntersecting) {
@@ -58,17 +77,21 @@ const skillItemsObserver = new IntersectionObserver((entries) => {
 	});
 });
 
-window.onload = () => {
-	Promise.all([...document.images].map((img) => (img.decode ? img.decode().catch(() => {}) : Promise.resolve()))).then(() => {
-		displayPage();
-
-		fullScroll = Math.round(document.documentElement.scrollHeight - document.documentElement.clientHeight);
-		headerHeight = document.querySelector('header').offsetHeight;
-		document.querySelectorAll('.section-title').forEach((section) => {
-			sectionPositions.push(section.offsetTop - headerHeight);
-		});
-		sectionPositions[4] = sectionPositions[4] - window.innerHeight;
+const moveLeftObserver = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) entry.target.classList.add('move-left');
 	});
+});
+
+window.onload = () => {
+	displayPage();
+
+	fullScroll = Math.round(document.documentElement.scrollHeight - document.documentElement.clientHeight);
+	headerHeight = document.querySelector('header').offsetHeight;
+	document.querySelectorAll('.section-title').forEach((section) => {
+		sectionPositions.push(section.offsetTop - headerHeight);
+	});
+	sectionPositions[4] = sectionPositions[4] - window.innerHeight;
 };
 
 hamburgerEl.addEventListener('click', (e) => {
@@ -82,5 +105,9 @@ skillItemsEls.forEach((el) => {
 	skillItemsObserver.observe(el);
 });
 
+moveLeftObserver.observe(skillSectionEl);
+
 document.querySelector('.age').textContent = year - 2004;
 document.querySelector('.year').textContent = year;
+
+window.innerWidth <= 870 ? navbarLinksEls[navbarLinksEls.length - 1].classList.remove('btn') : '';
